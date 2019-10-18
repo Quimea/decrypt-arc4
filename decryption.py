4e4a654df5fde0924a24989907451535c13a6e21
Nabil Djaber <40780645+Quimea@users.noreply.github.com>
2019-10-18 00:06:47 -0600

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -5,13 +5,13 @@ from base64 import b64encode, b64decode
 key = raw_input("input key(if key is unknown, put ` as key: ")
 msg = raw_input("input encrypted base64 message: ")
 
-if key == "`" {
+if key == "`" :
 print("the encrypted message: %s" %  msg)
 for i in range(256):
   cipher = ARC4.new((bytes([i])))
   msg = cipher.decrypt(b64decode(msg))
   print("The secret message is: %s" % msg)
- } else {
+
+else :
 cipher = ARC4.new(key)
 msg = cipher.decrypt(b64decode(msg))
-}
