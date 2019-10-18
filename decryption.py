f395400160e0059c63a5409d1c94d3a54a3df8df
Nabil Djaber <40780645+Quimea@users.noreply.github.com>
2019-10-18 00:07:39 -0600

    Update decryption.py

--- decryption.py
+++ decryption.py
@@ -6,12 +6,12 @@ key = raw_input("input key(if key is unknown, put ` as key: ")
 msg = raw_input("input encrypted base64 message: ")
 
 if key == "`" :
-print("the encrypted message: %s" %  msg)
-for i in range(256):
-  cipher = ARC4.new((bytes([i])))
-  msg = cipher.decrypt(b64decode(msg))
-  print("The secret message is: %s" % msg)
+  print("the encrypted message: %s" %  msg)
+  for i in range(256):
+    cipher = ARC4.new((bytes([i])))
+    msg = cipher.decrypt(b64decode(msg))
+    print("The secret message is: %s" % msg)
 
 else :
-cipher = ARC4.new(key)
-msg = cipher.decrypt(b64decode(msg))
+  cipher = ARC4.new(key)
+  msg = cipher.decrypt(b64decode(msg))
